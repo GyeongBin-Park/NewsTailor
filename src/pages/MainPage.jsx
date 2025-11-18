8bddf802f9957f0e19ac48c5a0aac1b2f4b033a9
GyeongBin Park <bin10308@naver.com>
2025-11-18 19:22:52 +0900

    fix: MainPage 전체듣기 부분듣기

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 import { logout } from "../api/auth";
